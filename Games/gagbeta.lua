3956a1ca3d81b0f2c842c0e36ea7dcc2852be312
Ега_биб <130549159+Amo1224gus@users.noreply.github.com>
2025-05-19 20:29:56 +0300

    Update gagbeta.lua

--- Games/gagbeta.lua
+++ Games/gagbeta.lua
@@ -13,7 +13,7 @@ local localeId = player.LocaleId
 local isRussian = localeId == "ru-ru"
 
 local englishText = {
-    windowTitle = "Egas X! [0.09Rewrite]",
+    windowTitle = "Egas X! [0.09Rewr]",
     windowNote = "Enter the key to access the script.\n\nNo key? Join our Discord: https://discord.gg/cRbced9G",
     homeTabTitle = "Home",
     autoFarmTabTitle = "Auto Farm",
